ed04ac9b16e1ef88b4e4dfa5e9baf16eddbaf822
Guelfo Watterson <163322667+gwatterson@users.noreply.github.com>
2026-03-18 14:29:33 +0100

    Add files via upload

---
+++ complex_logic.py
@@ -0,0 +1,45 @@
+def process_data(data, flag1, flag2, flag3, mode, extra):
+    if flag1:
+        if data:
+            if flag2:
+                if mode == "a":
+                    if flag3:
+                        if extra:
+                            for item in data:
+                                if item > 0:
+                                    if item < 100:
+                                        result = item * 2
+                                    else:
+                                        result = item
+                                else:
+                                    result = 0
+                        else:
+                            result = -1
+                    else:
+                        result = -2
+                elif mode == "b":
+                    result = sum(data)
+                else:
+                    result = 0
+            else:
+                result = len(data)
+        else:
+            result = None
+    else:
+        result = False
+    return result
+
+
+def x(a, b, c, d, e, f, g, h, i, j):
+    t = 0
+    for q in range(a):
+        for w in range(b):
+            for r in range(c):
+                t += q * w * r
+    if d:
+        t += e
+    elif f:
+        t -= g
+    else:
+        t = h + i + j
+    return t
